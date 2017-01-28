1421c83ea9b0b77d93ac90506e78b90a370aaebf
pacharanero <marcusbaw@gmail.com>
2017-01-28 12:00:44 +0000

    initial

---
+++ emergency/__init__.py
@@ -0,0 +1,13 @@
+"""
+emergency - Our OPAL Application
+"""
+from opal.core import application
+
+class Application(application.OpalApplication):
+    flow_module   = 'emergency.flow'
+    javascripts   = [
+        'js/emergency/routes.js',
+        'js/opal/controllers/discharge.js',
+        # Uncomment this if you want to implement custom dynamic flows.
+        # 'js/emergency/flow.js',
+    ]
